ee02b95a8656c8fa5408be251ef2085d100b96f8
sholpan <sholpan1889@gmail.com>
2023-03-22 20:14:33 -0400

    First commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello World");
+            System.out.println("Hello the World");
             Thread.sleep(4000);
         }
     }
